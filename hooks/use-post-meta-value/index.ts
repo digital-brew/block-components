0fbd4af875820cf20b514d3a8bf848dbdceccf75
Fabian Kägy <mail@fabian-kaegy.de>
2024-05-06 09:31:56 +0200

    refactor usePostMetaValue hook to ts

--- hooks/use-post-meta-value/index.ts
+++ hooks/use-post-meta-value/index.ts
@@ -1,21 +1,21 @@
 import { useEntityProp } from '@wordpress/core-data';
 import { usePost } from '../use-post';
 
-export const usePostMetaValue = (metaKey) => {
+export const usePostMetaValue = (metaKey: string) => {
 	const { postId, postType } = usePost();
-	const [meta, setMeta] = useEntityProp('postType', postType, 'meta', postId);
+	const [meta, setMeta] = useEntityProp('postType', postType, 'meta', postId as string);
 
 	if (!meta || !metaKey || !Object.prototype.hasOwnProperty.call(meta, metaKey)) {
 		return [undefined, () => {}];
 	}
 
 	const metaValue = meta[metaKey];
-	const setMetaValue = (newValue) => {
+	const setMetaValue = (newValue: any) => {
 		setMeta({
 			...meta,
 			[metaKey]: newValue,
 		});
 	};
 
-	return [metaValue, setMetaValue];
+	return [metaValue, setMetaValue] as const;
 };

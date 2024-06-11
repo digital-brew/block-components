0d0fca19a0da49a28d4f63f51aa212449aee59c9
Fabian Kägy <mail@fabian-kaegy.de>
2024-06-11 12:05:20 +0200

    remove defaultProps & propTypes from PostCategoryList

--- components/post-category-list/index.js
+++ components/post-category-list/index.js
@@ -1,14 +1,8 @@
-import PropTypes from 'prop-types';
 import { __ } from '@wordpress/i18n';
 import { PostTermList } from '../post-term-list';
 
-export const PostCategoryList = PostTermList;
-
-PostCategoryList.propTypes = {
-	taxonomyName: PropTypes.string,
-};
-
-PostCategoryList.defaultProps = {
-	taxonomyName: 'category',
-	noResultsMessage: __('Please select a category', 'tenup'),
-};
+export const PostCategoryList = ({
+	taxonomyName = 'category',
+	noResultsMessage = __('Please select a category', 'tenup'),
+	...rest
+}) => <PostTermList taxonomyName={taxonomyName} noResultsMessage={noResultsMessage} {...rest} />;

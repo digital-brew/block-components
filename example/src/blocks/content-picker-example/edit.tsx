76e6d815779fd926db8e57d7985ac6d93c889b00
Aleksandar Atanasov <aatanasovdev@gmail.com>
2025-02-22 12:41:23 +0200

    Adjust the PickedItemPreviewComponent info

--- example/src/blocks/content-picker-example/edit.tsx
+++ example/src/blocks/content-picker-example/edit.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { __ } from '@wordpress/i18n';
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { PanelBody, Placeholder } from '@wordpress/components';
-
+import { decodeEntities } from '@wordpress/html-entities';
+import { safeDecodeURI, filterURLForDisplay } from '@wordpress/url';
 import { ContentPicker } from '@10up/block-components';
+
+const PickedItemPreview = ({ item }) => {
+	return (
+		<a href={safeDecodeURI(item.url) || ''} target="_blank">
+			{decodeEntities(item.title)}
+		</a>
+	)
+};
 
 export const BlockEdit = (props) => {
 	const {
@@ -40,6 +49,7 @@ export const BlockEdit = (props) => {
 						fetchInitialResults
 						content={selectedPosts}
 						maxContentItems={5}
+						PickedItemPreviewComponent={PickedItemPreview}
 					/>
 				</Placeholder>
 			</div>
